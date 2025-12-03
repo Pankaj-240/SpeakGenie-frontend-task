e2954c86861968b95feec6e695f387800fa6b935
Pankaj Bisht <work.pankajbisht@gmail.com>
2025-12-03 22:14:38 +0530

    Second commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+
+const App = () => {
+  return (
+    <div>
+    <nav>
+      <Navbar/>
+    </nav>
+    <main>
+
+    </main>
+    <footer>
+
+    </footer>      
+    </div>
+
+  )
+}
+
+export default App
