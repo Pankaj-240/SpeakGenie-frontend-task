639611e0d6362139ab4b62a99895c2b89789fa0b
Pankaj Bisht <work.pankajbisht@gmail.com>
2025-12-06 23:35:54 +0530

    fix code

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Home from './pages/home/Home'
 import Quiz from './pages/custom_quiz/Quiz'
 import {Routes,Route} from 'react-router-dom'
 import Player from './pages/audio_story_player/Player'
-import Main from './components/main'
+import Main from './components/Main'
 const App = () => {
   return (
     <div>
